3135eea1bf5acace3ab5bb8e60f1fe4dad1ffd99
Karyuhin Sergej <karyuhin.sergej@gmail.com>
2019-10-03 19:59:55 +0300

    feat: task completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 module.exports = function zeros(expression) {
-  // your solution
+  let arr = expression.split("*");
+  let countTwo = 0;
+  let countFive = 0;
+  let base = 0;
+
+  arr.forEach(elem => {
+    base = elem.match(/!/g);
+    base = base.length;
+    elem = elem.match(/\d+/);
+    for (elem; elem > 0; elem -= base) {
+      let temp = elem;
+      while (temp % 5 == 0) {
+        temp = temp / 5;
+        countFive++;
+      }
+      while (temp % 2 == 0) {
+        temp = temp / 2;
+        countTwo++;
+      }
+    }
+  });
+
+  if (countFive > countTwo) {
+    return countTwo;
+  }
+  else {
+    return countFive;
+  }
 }
